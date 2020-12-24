d1e3aaabf02dec7276ac262dcf3c8a836bc91e22
Ty <turret1226@gmail.com>
2020-12-24 14:22:01 -0700

    Made the stringLeft() function follow my coding style better.

--- mapCounter.py
+++ mapCounter.py
@@ -7,7 +7,7 @@
 import os
 import sys
 
-def string_left(s,amount):
+def stringLeft(s,amount):
 	""""Removes the given number of characters from the string (s).
 	Emulates a BGT function to do this."""
 	return s[:amount]
@@ -27,29 +27,29 @@ f.close()
 platforms=walls=staircases=signs=doors=travelpoints=zones=sources=belts=vanishes=checkpoints=hazards=0
 final_text=text.split("\n")
 for i in final_text:
-	if string_left(i,8)=="platform" and len(i)>=16:
+	if stringLeft(i,8)=="platform" and len(i)>=16:
 		platforms+=1
-	if string_left(i,4)=="sign" and len(i)>=10:
+	if stringLeft(i,4)=="sign" and len(i)>=10:
 		signs+=1
-	if string_left(i,4)=="door" and len(i)>=12:
+	if stringLeft(i,4)=="door" and len(i)>=12:
 		doors+=1
-	if string_left(i,11)=="travelpoint" and len(i)>=18:
+	if stringLeft(i,11)=="travelpoint" and len(i)>=18:
 		travelpoints+=1
-	if string_left(i,12)=="sound_source" and len(i)>=18:
+	if stringLeft(i,12)=="sound_source" and len(i)>=18:
 		sources+=1
-	if string_left(i,4)=="belt" and len(i)>=12:
+	if stringLeft(i,4)=="belt" and len(i)>=12:
 		belts+=1
-	if string_left(i,4)=="zone" and len(i)>=12:
+	if stringLeft(i,4)=="zone" and len(i)>=12:
 		zones+=1
-	if string_left(i,9)=="staircase" and len(i)>=16:
+	if stringLeft(i,9)=="staircase" and len(i)>=16:
 		staircases+=1
-	if string_left(i,4)=="wall" and len(i)>=8:
+	if stringLeft(i,4)=="wall" and len(i)>=8:
 		walls+=1
-	if string_left(i,6)=="hazard" and len(i)>=12:
+	if stringLeft(i,6)=="hazard" and len(i)>=12:
 		hazards+=1
-	if string_left(i,10)=="checkpoint" and len(i)>=14:
+	if stringLeft(i,10)=="checkpoint" and len(i)>=14:
 		checkpoints+=1
-	if string_left(i,9)=="vanishing" and len(i)>=16:
+	if stringLeft(i,9)=="vanishing" and len(i)>=16:
 		vanishes+=1
 print("You have "+str(platforms)+" platforms, "+str(walls)+" walls, "+str(staircases)+" staircases, "+str(doors)+" doors, "+str(signs)+" signs, "+str(zones)+" zones, "+str(sources)+" sound sources, "+str(travelpoints)+" travelpoints, "+str(belts)+" conveyer belts, "+str(hazards)+" hazards, and "+str(vanishes)+" vanishing platforms.")
 sys.exit()
